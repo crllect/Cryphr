e2a2cec9ba155c77398096f2295f71b5018345e2
Crllect <144494583+crllect@users.noreply.github.com>
2023-12-25 10:57:14 -0500

    added credits, not done tho

--- js/clock.js
+++ js/clock.js
@@ -1,14 +1,27 @@
+let toggledClock = false;
+
+document.getElementById('time').addEventListener('click', function() {  // is the logic for clicking the clock making it 24 hour time, or standart time
+    toggledClock = !toggledClock;
+    updateClock();
+});
+
 function updateClock() {
     const now = new Date();
     let hours = now.getHours();
     const minutes = now.getMinutes().toString().padStart(2, '0');
     const seconds = now.getSeconds().toString().padStart(2, '0');
-    if (hours > 12) {
-        hours -= 12;
+    if (toggledClock) {
+        hours = hours.toString().padStart(2, '0');
+    } else {
+        if (hours > 12) {
+            hours -= 12;
+            }
+        if (hours === 0) {
+            hours = 12;
+        }
+        hours = hours.toString();
     }
-    if (hours === 0) {
-        hours = 12;
-    }
+
     const timeString = `${hours}:${minutes}:${seconds}`;
     document.getElementById('time').textContent = timeString;
   }
