baf6ef1ccaa0be4ab57c460326e51b451534330d
Crllect <144494583+crllect@users.noreply.github.com>
2023-12-27 14:47:32 -0500

    it kinda works. Huge thanks to rift for this

--- public/js/UVtest.js
+++ public/js/UVtest.js
@@ -1,5 +1,7 @@
 document.getElementById('myForm').onsubmit = function(event) {
     event.preventDefault();
-    var url = "youtube.com";
-    location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+    var url = "https://youtube.com"; // make sure to always add https://
+    var encodedUrl = __uv$config.encodeUrl(url);
+
+    document.getElementById('iframeWindow-1').src = __uv$config.prefix + encodedUrl;
 };
