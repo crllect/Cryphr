fb398b1ce8546487145a27b49e97f1219c974f05
Crllect <144494583+crllect@users.noreply.github.com>
2024-01-02 01:11:03 -0500

    adding settings page, WIP

--- public/js/waybar.js
+++ public/js/waybar.js
@@ -1,3 +1,41 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    for (let i = 0; i < localStorage.length; i++) {
+        let key = localStorage.key(i);
+        let value = localStorage.getItem(key);
+
+        if (key.startsWith("--")) {
+            document.documentElement.style.setProperty(key, value);
+        }
+    }
+});
+
+window.addEventListener(
+    "message",
+    function (event) {
+        var data = event.data;
+        if (data.variable && data.value) {
+            document.documentElement.style.setProperty(
+                data.variable,
+                data.value
+            );
+            localStorage.setItem(data.variable, data.value);
+        }
+    },
+    false
+);
+
+window.addEventListener('message', function(event) {
+    if (event.origin !== "*") {
+        return;
+    }
+
+    var data = event.data;
+    if(data.variable && data.value) {
+        document.documentElement.style.setProperty(data.variable, data.value);
+        localStorage.setItem(data.variable, data.value);
+    }
+}, false);
+
 let historyArray = JSON.parse(localStorage.getItem("historyArray")) || [];
 let nextHistoryItemId =
     JSON.parse(localStorage.getItem("nextHistoryItemId")) || 0;
