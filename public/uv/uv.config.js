59722a71b954ac4c695599a5578eec9b2c307525
Crllect <144494583+crllect@users.noreply.github.com>
2024-01-03 09:32:35 -0500

    Leaching off of a bare I found online until I buy my own, prolly like a week or two

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,7 +1,7 @@
 /*global Ultraviolet*/
 self.__uv$config = {
     prefix: '/uv/service/',
-    bare: 'https://nebulaproxy.io/bare/',
+    bare: 'https://engineering.scarat3k.me/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
