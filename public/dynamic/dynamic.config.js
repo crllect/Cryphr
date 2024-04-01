e024994f5da29ed7c75f89c4867c061f5f7dea98
Crllect <144494583+crllect@users.noreply.github.com>
2024-04-01 13:21:23 +0000

    switched to focus bare

--- public/dynamic/dynamic.config.js
+++ public/dynamic/dynamic.config.js
@@ -5,7 +5,7 @@ self.__dynamic$config = {
     logLevel: 3,
     bare: {
         version: 2,
-        path: "https://roblox-interstellar.vercel.app/outerspace/",
+        path: "https://focus-bare.vercel.app/",
     },
     tab: {
         title: "Service",
--- server.js
+++ server.js
@@ -36,7 +36,7 @@ server.on("upgrade", (req, socket, head) => {
 
 let port = parseInt(process.env.PORT || "");
 
-if (isNaN(port)) port = 443;
+if (isNaN(port)) port = 3000;
 
 server.on("listening", () => {
     const address = server.address();
