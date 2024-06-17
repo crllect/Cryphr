80674ab65a17379999f688293a809d5c6b30aa56
Gareth Flandro <137940955+divnerd@users.noreply.github.com>
2024-06-17 18:41:21 -0400

    Update menu.js w/ comments
    Added some brief overview comments for readability and ease of debugging purposes

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,21 +1,29 @@
 document.querySelector('.menu').addEventListener('click', function () {
+	// Toggle the 'menuOpen' class on the menu element
 	this.classList.toggle('menuOpen');
+
+	// Get class lists of various elements for easy manipulation
 	var mainWindow = document.querySelector('.mainWindow').classList;
 	var iframeWindow = document.querySelector('.iframeWindow').classList;
 	var waybar = document.querySelector('.waybar').classList;
 	var fullMenu = document.querySelector('.fullMenu').classList;
 	var body = document.body.classList;
+
+	// If the menu is open
 	if (this.classList.contains('menuOpen')) {
+		// Add 'menuOpen' related classes to elements for styling
 		mainWindow.add('menuOpen-mainWindow');
 		iframeWindow.add('menuOpen-iframeWindow');
 		waybar.add('menuOpen-waybar');
 		fullMenu.add('menuOpen-fullMenu');
 		body.add('menuOpen-body');
 
+		// If the iframeWindow is not hidden, add an additional class 
 		if (!iframeWindow.contains('hidden')) {
 			iframeWindow.add('menuOpen-iframeWindowOpen');
 		}
 	} else {
+		// Remove 'menuOpen' related classes from elements
 		mainWindow.remove('menuOpen-mainWindow');
 		iframeWindow.remove('menuOpen-iframeWindow');
 		iframeWindow.remove('menuOpen-iframeWindowOpen');
@@ -23,10 +31,9 @@ document.querySelector('.menu').addEventListener('click', function () {
 		fullMenu.remove('menuOpen-fullMenu');
 		body.remove('menuOpen-body');
 
+		// If the iframeWindow is not hidden, add 'iframeWindowOpen' class
 		if (!iframeWindow.contains('hidden')) {
 			iframeWindow.add('iframeWindowOpen');
 		}
 	}
 });
-
-// make sure that every element is accounted for, and that it accounts for active elements, you will figure something out
